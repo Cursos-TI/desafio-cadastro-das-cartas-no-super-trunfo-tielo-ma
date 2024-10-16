b2f0cd9b64b1e08a8de274ad703034a54837fd2e
Marcelo <90ma.celo@gmail.com>
2024-10-16 17:19:31 +0000

    Aqui acaba o meu código no modo iniciante.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,6 +12,18 @@ int main() {
     char codigo;
     int npt;
     float pop, area, pib;
+    /*OBS: AQUI ESTAREI DEIXANDO O CÓDIGO RESPECTIVO DE CADA ESTADO E CIDADE
+    ESTADOS              | CIDADES
+    A - ACRE             | A1 - Acrelândia     | A2 - Feijó          | A3 - Jordão           | A4 - Rio Branco
+    B - BAHIA            | B1 - Salvador       | B2 - Itabuna        | B3 - Feira de Santana | B4 - Vitória da Conquista
+    C - CEARÁ            | C1 - Fortaleza      | C2 - Itapipoca      | C3 - Aracati          | C4 - Sobral
+    D - DISTRITO FEDERAL | D1 - Brasília       | D2 - Ceilândia      | D3 - Sobradinho       | D4 - Gama
+    E - ESPÍRITO SANTO   | E1 - Vitória        | E2 - Linhares       | E3 - Serra            | E4 - Vila Velha
+    F - SÃO PAULO        | F1 - São Paulo      | F2 - Ribeirão Preto | F3 - Diadema          | F4 - Guarulhos
+    G - RIO DE JANEIRO   | G1 - Rio de Janeiro | G2 - Niterói        | G3 - São Gonçalo      | G4 - Belford Roxo
+    H - MINAS GERAIS     | H1 - Belo Horizonte | H2 - Betim          | H3 - Montes Claros    | H4 - Uberlândia
+    */
+   printf("========== SUPER TRUNFO - PAÍSES ========= \n");
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
